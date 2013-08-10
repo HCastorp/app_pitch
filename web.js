545143d8c556e829a54fe22005f92213b3ae4d63
Sam Westley <swwestley@gmail.com>
2013-08-10 21:15:47 +0000

    ready for testing via heroku

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+var app = express.createServer(express.logger());
+
+app.use(express.static(__dirname));
+
+app.get('/', function(request, response) {
+  var mybuf = fs.readFileSync("index.html");
+  response.send(mybuf.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
